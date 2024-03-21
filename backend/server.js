42f8724860717e3c376a78c7d3a945201227ca4c
ajzal-byte <ajzalaju10@gmail.com>
2024-03-21 14:12:00 +0530

    routes set

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!")
+})
+
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`));
