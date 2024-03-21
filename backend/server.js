1d7307b05a397072edb09e6a6c53e3d3376c683e
ajzal-byte <ajzalaju10@gmail.com>
2024-03-21 15:00:05 +0530

    mongodb connection implemented

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world!")
-})
+  res.send("Hello world!");
+});
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`));
+app.listen(PORT, async () => {
+  await connectToMongoDB();
+  console.log(`Server started listening on port ${PORT}`);
+});
