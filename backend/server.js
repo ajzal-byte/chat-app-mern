cd9e579f68fc802c2fad241461f9ce4ff2558515
ajzal-byte <ajzalaju10@gmail.com>
2024-03-21 12:33:20 +0530

    dotenv configured

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => console.log("Server started listening on port 5000"))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`));
