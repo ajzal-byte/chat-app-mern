a7931c8cb82db84955a196d55c681377b86600fc
ajzal-byte <ajzalaju10@gmail.com>
2024-03-23 12:15:32 +0530

    retrieve messages implemented

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -27,16 +27,36 @@ export const sendMessage = async (req, res) => {
     if (newMessage) conversation.messages.push(newMessage._id);
 
     // SOCKET IO FUNCTIONALITY
-  
+
     // await conversation.save();
     // await newMessage.save();
 
     // to run concurrently
-    await Promise.all([conversation.save(), newMessage.save()])
+    await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
   } catch (error) {
     console.error("Error while sending message", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error while getting messages", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { sendMessage } from "../controller/message.controller.js";
+import { sendMessage, getMessages } from "../controller/message.controller.js";
 
 const router = express.Router();
 
+router.get("/:id", protectRoute, getMessages)
 router.post("/send/:id", protectRoute, sendMessage)
 
 export default router
