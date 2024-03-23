08336e19d278474acc745f0283905bf8bf91b245
ajzal-byte <ajzalaju10@gmail.com>
2024-03-23 18:24:52 +0530

    Signup ui completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Login } from "./pages";
+import { Login, SignUp } from "./pages";
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      {/* <Login /> */}
+      <SignUp />
     </div>
   );
 };
