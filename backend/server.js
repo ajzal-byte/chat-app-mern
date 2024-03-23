f89ec2ff37254cef49868deb4f881b6776a19f05
ajzal-byte <ajzalaju10@gmail.com>
2024-03-23 13:33:28 +0530

    user router set up

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 dotenv.config();
 
 const app = express();
@@ -15,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.use(express.urlencoded({ extended: false }));
 
