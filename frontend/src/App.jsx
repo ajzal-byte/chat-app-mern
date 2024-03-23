0ae30e097e3660deda97359152f049388d0b2e97
ajzal-byte <ajzalaju10@gmail.com>
2024-03-24 05:18:46 +0530

    Toaster added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home, Login, SignUp } from "./pages";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
