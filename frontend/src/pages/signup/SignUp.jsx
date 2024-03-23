e00fac2d45f141fe3fc467fb5097c1bfd152b30a
ajzal-byte <ajzalaju10@gmail.com>
2024-03-23 19:58:59 +0530

    code formatted

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -3,7 +3,10 @@ import GenderCheckbox from "./GenderCheckbox";
 const SignUp = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <div
+        className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter 
+      backdrop-blur-lg bg-opacity-0"
+      >
         <h1 className="text-3xl font-semibold text-center text-gray-300">
           SignUp
           <span className="text-green-300"> Bello</span>
@@ -50,9 +53,12 @@ const SignUp = () => {
             />
           </div>
 
-          <GenderCheckbox/>
+          <GenderCheckbox />
 
-          <a href="#" className="text-sm hover:underline hover:text-green-300 mt-2 inline-block">
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-green-300 mt-2 inline-block"
+          >
             Already have an account?
           </a>
           <div>
