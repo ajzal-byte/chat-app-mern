c1b09fd6b16360ce5fcd379ec3b41a9d61e54be6
ajzal-byte <ajzalaju10@gmail.com>
2024-03-23 23:52:08 +0530

    Link to signup added

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
@@ -31,12 +33,12 @@ const Login = () => {
               className="w-full input input-bordered h-10"
             />
           </div>
-          <a
+          <Link to='/signup'
             href="#"
             className="text-sm hover:underline hover:text-green-300 mt-2 inline-block p-2"
           >
             Don't have an account?
-          </a>
+          </Link>
           <button className="btn btn-block btn-sm mt-2">Login</button>
         </form>
       </div>
