3c6a598800045b7ffe3ef2e08052bebe771c266a
ajzal-byte <ajzalaju10@gmail.com>
2024-03-23 20:11:08 +0530

    code formatted

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -4,10 +4,13 @@ const Conversation = () => {
       <div className="flex gap-2 items-center hover:bg-green-700 rounded p-2 py-1 cursor-pointer">
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCUe9wnnxcH6NBN-4j1oDzYPgBcEKdJgj6A" alt="user avatar" />
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCUe9wnnxcH6NBN-4j1oDzYPgBcEKdJgj6A"
+              alt="user avatar"
+            />
           </div>
         </div>
-        
+
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
             <p className="font-bold text-gray-200">John Doe</p>
@@ -18,7 +21,7 @@ const Conversation = () => {
 
       <div className="divider my-0 py-0 h-1" />
     </>
-  )
-}
+  );
+};
 
-export default Conversation
+export default Conversation;
