42bf32c22ab3adff359e0ad9e7aa016f7ecf00a0
ajzal-byte <ajzalaju10@gmail.com>
2024-03-23 18:30:11 +0530

    code formatted

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,10 @@
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <div
+        className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding 
+      backdrop-filter backdrop-blur-lg bg-opacity-0"
+      >
         <h1 className="text-3xl font-semibold text-center text-gray-300">
           Login
           <span className="text-green-300"> Bello</span>
