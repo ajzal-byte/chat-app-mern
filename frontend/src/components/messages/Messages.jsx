1a547040cf3b11b1c70a32d191575acbe809bc57
ajzal-byte <ajzalaju10@gmail.com>
2024-03-24 21:37:06 +0530

    auto scroll to last message

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,24 @@
 import { Message } from "../";
 import useGetMessages from "../../hooks/useGetMessages";
 import { MessageSkeleton } from "../";
+import { useEffect, useRef } from "react";
 
 const Messages = () => {
   const { loading, messages } = useGetMessages();
-  console.log("Messages: ", messages);
+  const lastMessageRef = useRef(null);
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 50)
+  }, [messages]);
   return (
     <div className="px-4 flex-1 overflow-auto max-w-screen-sm">
       {!loading &&
         messages.length > 0 &&
         messages.map((message, id) => (
-          <Message key={message._id} message={message} />
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
         ))}
 
       {loading && [...Array(3)].map((_, i) => <MessageSkeleton key={i} />)}
