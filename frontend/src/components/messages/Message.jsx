cc8da33e22ea08367da63ab3141a2a7ebc488b3b
ajzal-byte <ajzalaju10@gmail.com>
2024-03-24 21:19:35 +0530

    msg send time displayed

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,5 +1,6 @@
 import { useAuthContext } from "../../context/AuthContext";
 import useConversation from "../../zustand/useConversation";
+import { extractTime } from "../../utils/extractTime";
 
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
@@ -10,18 +11,21 @@ const Message = ({ message }) => {
     ? authUser.profilePic
     : selectedConversation.profilePic;
   const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const formattedTime = extractTime(message.createdAt);
+
   return (
     <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            src={profilePic}
-            alt="user avatar"
-          />
+          <img src={profilePic} alt="user avatar" />
         </div>
       </div>
-      <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1">9:41</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
+      </div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1">
+        {formattedTime}
+      </div>
     </div>
   );
 };
