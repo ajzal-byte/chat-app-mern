6bb824016f4638a2c4f47a4bc22d856b0f6532db
ajzal-byte <ajzalaju10@gmail.com>
2024-03-24 20:20:24 +0530

    messages retrieved

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,6 +1,9 @@
 import { Message } from "../";
+import useGetMessages from "../../hooks/useGetMessages";
 
 const Messages = () => {
+  const { loading, messages } = useGetMessages();
+  console.log("Messages: ", messages);
   return (
     <div className="px-4 flex-1 overflow-auto max-w-screen-sm">
       <Message />
