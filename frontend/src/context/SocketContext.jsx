9b14c2991b0cf5047c946d8d48cf01d5bd0ab9ed
ajzal-byte <ajzalaju10@gmail.com>
2024-03-25 03:18:53 +0530

    bug fixed (user status updation)

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -32,7 +32,7 @@ export const SocketContextProvider = ({ children }) => {
         setSocket(null);
       }
     }
-  }, []);
+  }, [authUser]);
   return (
     <SocketContext.Provider value={{ socket, onlineUsers }}>
       {children}
