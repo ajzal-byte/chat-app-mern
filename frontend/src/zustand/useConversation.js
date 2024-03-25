bb26ea4b85bc8fd8c8f63205736702930bdf45a2
ajzal-byte <ajzalaju10@gmail.com>
2024-03-26 02:03:15 +0530

    selectedConversation typo fixed

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 const useConversation = create((set) => ({
-  selectedConversaion: null,
+  selectedConversation: null,
   setSelectedConversation: (selectedConversation) =>
     set({ selectedConversation }),
   messages: [],
